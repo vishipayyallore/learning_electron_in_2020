c38b718df90750a338f1583716ea2d12fd009bf0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-04-02 22:53:34 +0530

    ..

--- second-electron-app/renderer.js
+++ second-electron-app/renderer.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const ipcRenderer = require('electron').ipcRenderer; 
+
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process because
@@ -13,5 +15,5 @@ btnSum.addEventListener('click', function(){
     const number1 = document.getElementById('number1');
     const number2 = document.getElementById('number2');
 
-    alert(`Sum: ${number1.value + number2.value}`);
+    ipcRenderer.send("btnSum", number2.value);
 });
