ae76dcce43397783bfa1a73387e2417a6ed4bc46
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-04-02 22:48:14 +0530

    ..

--- second-electron-app/renderer.js
+++ second-electron-app/renderer.js
@@ -1,6 +1,17 @@
+'use strict';
+
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process because
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const btnSum = document.getElementById('btnSum');
+btnSum.addEventListener('click', function(){
+
+    const number1 = document.getElementById('number1');
+    const number2 = document.getElementById('number2');
+
+    alert(`Sum: ${number1.value + number2.value}`);
+});
